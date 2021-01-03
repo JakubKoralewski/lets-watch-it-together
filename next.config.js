8d57ba7307b9a90bd1c24c04116ae20d1090f68a
Jakub Koralewski <jakkor8@st.amu.edu.pl>
2021-01-03 12:48:52 +0100

    Try to fix internal server error on heroku review app
    I think it is because the NEXTAUTH_URL variable is not set, but idk

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withPWA = require('next-pwa')
 
 module.exports = (_phase, {_defaultConfig}) => {
 	return withPWA({
+		env: process.env.HEROKU_APP_NAME ? {
+			NEXTAUTH_URL: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`,
+		} : undefined,
 		pwa: {
 			dest: 'public',
 			disable: process.env.NODE_ENV === 'development'
